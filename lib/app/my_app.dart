e72ca019f25350a564ab2e83c58ad9c73d24e436
Carla <carlasantos45@hotmail.com.br>
2024-06-17 23:10:49 -0300

    Organização do projeto

---
+++ lib/app/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_agenda/app/view/my_home_page.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Agenda - Curso Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
